474f3ad6fe42808a34db86c31af54caa5e1db0e6
Gauri Patil <gauri.be0811@gmail.com>
2023-05-18 21:05:24 +0530

    Exploring loose coupling with Interfaces - PacManGame

--- game-runner-app/src/main/java/com/mylearning/gamerunnerapp/GameRunnerAppApplication.java
+++ game-runner-app/src/main/java/com/mylearning/gamerunnerapp/GameRunnerAppApplication.java
@@ -2,6 +2,7 @@ package com.mylearning.gamerunnerapp;
 
 import com.mylearning.gamerunnerapp.game.GameRunner;
 import com.mylearning.gamerunnerapp.game.MarioGame;
+import com.mylearning.gamerunnerapp.game.PacManGame;
 import com.mylearning.gamerunnerapp.game.SuperContraGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +12,18 @@ public class GameRunnerAppApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(GameRunnerAppApplication.class, args);
-//		MarioGame game = new MarioGame();
-		SuperContraGame game = new SuperContraGame();
+		MarioGame game = new MarioGame();
+		SuperContraGame superContraGame = new SuperContraGame();
+		PacManGame pacManGame = new PacManGame();
+
 		GameRunner runner = new GameRunner(game);
 		runner.run();
+
+		GameRunner superContraGameRunner = new GameRunner(superContraGame);
+		superContraGameRunner.run();
+
+		GameRunner pacManGameRunner = new GameRunner(pacManGame);
+		pacManGameRunner.run();
 	}
 
 }
---
+++ game-runner-app/src/main/java/com/mylearning/gamerunnerapp/game/PacManGame.java
@@ -0,0 +1,19 @@
+package com.mylearning.gamerunnerapp.game;
+
+public class PacManGame implements GamingConsole {
+    public void up() {
+        System.out.println("PacMan jump");
+    }
+
+    public void down() {
+        System.out.println("PacMan down into hole");
+    }
+
+    public void left() {
+        System.out.println("PacMan stop");
+    }
+
+    public void right() {
+        System.out.println("PacMan accelerate");
+    }
+}
