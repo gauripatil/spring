f3e13c35445fff0c892e7559c6bd246bbc9194ae
Gauri Patil <gauri.be0811@gmail.com>
2023-05-20 23:55:38 +0530

    Spring framework - Understanding Important Terminology

--- game-runner-app/src/main/java/com/mylearning/gamerunnerapp/GameRunnerAppApplication.java
+++ game-runner-app/src/main/java/com/mylearning/gamerunnerapp/GameRunnerAppApplication.java
@@ -32,3 +32,19 @@ public class GameRunnerAppApplication {
 	}
 
 }
+
+/**
+ * NOTES:
+ * 1. @Component: A Class managed by Spring framework
+ * 2. Dependency: GameRunner needs GamingConsole impl.
+ * 	2.1 GamingConsole (Ex. MarioGame) is dependency of GameRunner
+ * 3. Component Scan: How does Spring framework find component classes?
+ * 	3.1 It scans packages: (@ComponentScan("packagename"))
+ * 4. Dependency Injection: Identify beans, their dependencies and wire them together
+ *    (provide IOC - Inversion Of Control)
+ *  4.1 Spring Beans: An object managed by Spring framework
+ *  4.2 IoC Container: Manages the lifecycle of beans and dependencies
+ *  	4.2.1 Types: ApplicationContext(complex), BeanFactory(simple, rarely used)
+ *  4.3 Autowiring: Process of wiring in dependencies for Spring Bean
+ *
+ */
